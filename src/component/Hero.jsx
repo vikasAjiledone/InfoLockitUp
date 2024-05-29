db604f517651ac307339ef796d85d434c84c6f46
vikasAjiledone <vikas.pandey@ajiledone.com>
2024-05-29 23:58:08 +0530

    logo fix in mobile

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -77,7 +77,7 @@ const Hero = () => {
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  src={logoImg}
                   alt=""
                 />
               </a>
